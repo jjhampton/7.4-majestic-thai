55664e2c4323682234800f27a1d9f34bd2ecd9ea
John Hampton <john_j_hampton@yahoo.com>
2015-06-18 17:07:25 -0400

    Use the renderChildren method to render a child view for each food item

--- scripts/views/categoryItemView.js
+++ scripts/views/categoryItemView.js
@@ -1,10 +1,21 @@
 export default Backbone.View.extend({
   template: JST.categoryItem,
-  className: category-item,
+  className: 'category-item',
+
+  initialize: function() {
+    this.render();
+  },
+
+  render: function() {
+    this.$el.html(this.template(this.model.toJSON()));
+  }
 
 });
 
 
+
+// <section class="category">
 // <h2 class="category-title">Popular Items<i class="category-dropdown fa fa-sort-desc"></i></h2>
+// </section>
 
 //will need that later for when implement categories ^
--- scripts/views/menuView.js
+++ scripts/views/menuView.js
@@ -1,5 +1,6 @@
+import CategoryItemView from './categoryItemView';
+
 export default Backbone.View.extend({
-  template: JST.menu,
   tagName: 'section',
   className: 'menu',
 
@@ -8,6 +9,25 @@ export default Backbone.View.extend({
   },
 
   render: function() {
-    this.$el.html(this.template(this.collection.toJSON()));
+    this.renderChildren();
+  },
+
+  renderChildren: function() {
+    _.invoke(this.children || [], 'remove');
+
+    this.children = this.collection.map(function(child) {
+      var view = new CategoryItemView({
+        model: child,
+      });
+      this.$el.append(view.el);
+      return view;
+    }.bind(this));
+
+    return this;
+  },
+
+  remove: function(){
+    _.invoke(this.children || [], 'remove');
+    Backbone.View.prototype.remove.apply(this, arguments);
   }
 });
